6cebf02117eea46adf06c6ee3a9e800b6422daf1
Harshika Patel <harshikapatel2574@gmail.com>
2025-01-26 04:45:20 -0500

    update navigation related code in app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,57 @@ import Header from './components/Header/Header'
 import Hero from './components/Hero/Hero'
 import Photographs from './components/Photographs/Photographs'
 import Tags from './components/Tags/Tags';
-import { useState } from "react";
-
+import { API_KEY } from './Utils/api';
+import { API_URL } from './Utils/api';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import { useState ,useEffect} from "react";
+import axios from 'axios';
+import HomePage from './pages/HomePage/HomePage';
+import PhotoDetailsPage from './pages/PhotoDetailsPage/PhotoDetailsPage'
 function App() {
+ 
   const [isTagsPanelOpen, setIsTagsPanelOpen] = useState(false);
-  const [selectedTag, setSelectedTag] = useState(null);
+  const [photos,setPhotos]=useState([]);
+  const [loading,setLoading]=useState(true);
+  const[error,setError]=useState(null);
+  
+  
+  useEffect(()=>{
+    axios.get(`${API_URL}?api_key=${API_KEY}`)
+    .then((response)=>{
+      setPhotos(response.data);
+      setLoading(false);
+    })
+    .catch((err)=>{
+      console.error(err);
+      setError("error fetching photo");
+      setLoading(false);
+    })
+  },[])
+
+  if(loading) return(<div>Loading......</div>)
+    if (error) return(<div>{error}</div>)
+  
   const handleTogglePanel = () => {
    
     setIsTagsPanelOpen((prev) => !prev);
 };
   return (
     <>
-    <Header handleTogglePanel={handleTogglePanel} isTagsPanelOpen={isTagsPanelOpen} />
-    <main className="main">
     
-    {isTagsPanelOpen && 
-    <Tags selectedTag={selectedTag} setSelectedTag={setSelectedTag} />}
-    <Hero />
-    <Photographs selectedTag={selectedTag}/>
+   
+    <BrowserRouter>
+    <Header handleTogglePanel={handleTogglePanel}  />
+   
+    <main className="main">
+    <Routes>
+      <Route path="/" element={<HomePage isTagsPanelOpen={isTagsPanelOpen} photos={photos}/>}/>
+      <Route path="/:photoId" element={<PhotoDetailsPage photos={photos}/>}/>
+    </Routes>
     </main>
+    
     <Footer />
+    </BrowserRouter>
 
     </>
   )
