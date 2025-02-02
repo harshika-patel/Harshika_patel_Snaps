524226dbbc5e584465059d6af1ec757f8399a64b
Harshika Patel <harshikapatel2574@gmail.com>
2025-02-02 15:05:30 -0500

    Add server-side code

--- Harshika-Patel-snaps-api/index.js
+++ Harshika-Patel-snaps-api/index.js
@@ -7,11 +7,9 @@ dotenv.config();
 
 
 const app = express();
-
+const PORT = process.env.PORT || 8080;
 app.use(cors()); 
 app.use(express.json());
-
-const PORT = process.env.PORT || 8080;
 
 app.use('/tags', tagsRoutes);
 app.use('/photos', photosRoutes);
